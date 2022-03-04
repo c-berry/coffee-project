8b6519ca5157e83472bdee3e1e0843261b1539d0
Christopher Berry <christopher.m.berry01@gmail.com>
2022-03-03 20:14:33 -0600

    change table to divs

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 "use strict"
 
 function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
-    html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
-    html += '</tr>';
+    var html = '<div class="coffee">';
+    html += '<div class="d-none">' + coffee.id + '</div>';
+    html += '<div>' + coffee.name + '</div>';
+    html += '<p>' + coffee.roast + '</p>';
+    html += '</div>';
 
     return html;
 }
